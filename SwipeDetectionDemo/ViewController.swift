121f24dbcbc94cd0e5009b36513992cff7357afe
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-03-03 17:43:45 +0900

    print force to log

--- SwipeDetectionDemo/ViewController.swift
+++ SwipeDetectionDemo/ViewController.swift
@@ -10,30 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
   
-  @IBOutlet fileprivate weak var detectionView: ForceTouchDetectionView!
+  @IBOutlet private weak var detectionView: ForceTouchDetectionView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
 
   }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    checkForceTouchCapability()
+  }
+  
+  // MARK: - 
+  private func checkForceTouchCapability() {
+    
+    guard view.traitCollection.forceTouchCapability == .available else {
+      
+      let alertController = UIAlertController(title: "起動できません", message: "3Dタッチをサポートしていない\nデバイスのようです", preferredStyle: .alert)
+      
+      present(alertController, animated: true, completion: nil)
+      return
+    }
+  }
 }
 
 class ForceTouchDetectionView: UIView {
   
+  // MARK: - 
+  private func someEventHandling(touch: UITouch) {
+    print(touch.force)
+  }
+  
   // MARK: - UIControl Events
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    print(#function)
+    if let touch = touches.first {
+      someEventHandling(touch: touch)
+    }
+//    print(#function)
   }
   
   override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-    print(#function)
+    if let touch = touches.first {
+      someEventHandling(touch: touch)
+    }
+//    print(#function)
   }
   
   override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-    print(#function)
+//    print(#function)
   }
   
   override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-    print(#function)
+//    print(#function)
   }
 }
